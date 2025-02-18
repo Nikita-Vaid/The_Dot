39417ad78b85672f8458db41e3fb210e3b4d943f
RaviRanjan20002 <raviranjankumar130082002@gmail.com>
2025-02-18 16:56:04 +0530

    updated

--- src/Compenents/landingpage/TriumphSection.jsx
+++ src/Compenents/landingpage/TriumphSection.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import "../../../styles/TriumphSection.css";
 import truphImage from "../../assets/ASD.jpg";
-
+import first from "../../assets/4.jpg";
+import second from "../../assets/2.jpg";
+import third from "../../assets/3.jpg";
+import fourth from "../../assets/1.jpg";
 const TriumphSection = () => {
   const title = "THE TRIUMPH OF 5 YEARS";
   const subtitle = "25 AIR 1 in JEE & Pre-Medical all from classroom";
+  const bannerimg =[
+    first
+  ];
   const participants = [
     "Garv Gupta (CRL 2267)", "Mukul Kaushik (CRL 3810)", "Kuran Yadav (AIR 1851*)", 
     "Chetna Sharma (AIR 2313)", "Pooja Thakran (AIR 4422)", "Harshita (AIR 3149)", 
@@ -12,14 +18,17 @@ const TriumphSection = () => {
     "Dhruv Yadav (AIR 4039*)", "Harsh Lamba (AIR 5418)", "Isha Yadav (AIR 4067)", 
     "Kashish Kapadia (AIR 5376)", "Ravikant (AIR 3844)", "Nancy Yadav (AIR 5297)"
   ];
-  
-
+ 
   return (
     <div className="triumph-section">
       <h1 className="title">{title}</h1>
       <h2 className="subtitle">{subtitle}</h2>
       <div className="image-container">
         <img src={truphImage} alt="Group" className="group-image" />
+        {bannerimg.map((img,index) =>
+        <img src={img} alt="Group" className="group-image" key={index}/>
+        )}
+        
       </div>
       <div className="participants">
         <p>
