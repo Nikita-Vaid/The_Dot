0ee763745a501c2b5a8fe74e512f79341f8a7e68
Nikita-Vaid <vaidnikita06@gmail.com>
2025-02-20 16:04:39 +0530

    Result3

--- src/Compenents/landingpage/StatsSection.jsx
+++ src/Compenents/landingpage/StatsSection.jsx
@@ -3,11 +3,15 @@ import "../../../styles/StatsSection.css";
 
 const StatsSection = () => {
   const stats = [
-    { label: "Over 500 students mentored in 2024, fostering academic growth and success." },
-    { label: "Specialized coaching for JEE, NEET, and competitive exams, tailored to each student's needs." },
-    { label: "Proudly serving students in Rewari, with plans for expansion to other cities in India." },
-    { label: "In 2024, we welcomed over 1,000 students, reflecting our growing reputation for quality education." },
-    { label: "Strategic partnerships with leading educational platforms for a comprehensive learning experience." },
+    { label: "Highly Qualified Faculties" },
+    { label: "Experienced IITians Team In PCM." },
+    { label: "Faculty Stability." },
+    { label: "Individual Attention." },
+    { label: "Dedicated Student Counselling Team." },
+    { label: "Parents Direct Interaction With Faculties." },
+    { label: "Best Selection Ratio." },
+    { label: "Transparency Of Results." },
+    { label: "100% Results Verification." },
   ];
 
   return (
